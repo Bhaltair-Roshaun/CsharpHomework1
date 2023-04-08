9b67c4be1528a84b55f5bdf623943b70d98f3846
Bhaltair <gilliarool@mail.ru>
2023-04-08 17:03:37 +0300

    task #2 solved Слёзкин Андрей

--- Task #2/Задача 1.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Write("Введите число a: ");
-double a = double.Parse(Console.ReadLine());
-
-Console.Write("Введите число b: ");
-double b = double.Parse(Console.ReadLine());
-
-Console.Write("Максимальное число из двух вышеприведенных: " + Math.Max(a, b));
---
+++ Task #2/Задача 2.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введите число c: ");
+double c = double.Parse(Console.ReadLine());
+
+Console.Write("Максимальное число из трех вышеприведенных: " + Math.Max(a, Math.Max(b, c)));
