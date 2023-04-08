0218ebd550e273c85976c25ec612b85666467864
Bhaltair <gilliarool@mail.ru>
2023-04-08 17:00:01 +0300

    task #2 begin

---
+++ Task #2/Задача 1.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Максимальное число из двух вышеприведенных: " + Math.Max(a, b));
